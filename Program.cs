e0d00e086f08e7444dec963612ef2ff27e745740
PC <PC@DESKTOP-FJAUUMM>
2024-11-29 14:21:44 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+public class Repo
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    [JsonPropertyName("html_url")]
+    public string HtmlUrl { get; set; }
+
+    [JsonPropertyName("homepage")]
+    public string Homepage { get; set; }
+
+    [JsonPropertyName("watchers")]
+    public int Watchers { get; set; }
+
+    [JsonPropertyName("pushed_at")]
+    public DateTime PushedAt { get; set; }
+}
